fed942817124b71c229424a5795f4bf4bc404648
MostafaKandel <69434233+mahmoudanur@users.noreply.github.com>
2022-07-25 16:19:27 +0200

    new version

--- task.py
+++ task.py
@@ -1,23 +1,46 @@
-# to validate the Format of date and check if it's valied or no
-def validate_date(date):
-        day=date.split(".")
+import datetime
+import time
+ 
+# to validate the Format of date  and check if it's valied or no
+def validate_date(user_date):
+        day=user_date.split(".")
         if  int(day[0]) in range(1,32) and int(day[1]) in range(1,13) and int(day[2]) in range(1,2023)  :
                 return True
         else:
                 return False         
          
-                 
-         
-        
-               
 # to validate the Format of time and check if it's valied or no
 
-def validate_time(time):
-        clock=time.split(":")
-        if int(clock[0]) in range(1,25) and int(clock[1]) in range(1,61):
+def validate_time(user_time):
+        clock=user_time.split(":")
+        if int(clock[0]) in range(0,24) and int(clock[1]) in range(0,60):
                 return True
         else:
-                return False        
+                return False   
+
+# function of current date and time 
+def validate_current_date_time():
+        current = datetime.datetime.now()
+        time_now = current.strftime("%H:%M")
+        date = current.strftime("%d.%m.%Y")
+        current_datetime=" ".join([date,time_now])
+        current_datetime = date+ " - " +time_now
+        return current_datetime   
+
+#function to check which data is reached
+def print_result(counter):
+        if counter==1:
+            return "The first data has been reached"
+              
+        elif counter==2:
+               return "The second data has been reached"
+        elif counter==3:
+               return "The Third data has been reached"
+        else:
+               return "the {} date has been reached".format(counter)
+        
+                                    
+
          
                 
 if __name__ == '__main__':
@@ -25,23 +48,41 @@ if __name__ == '__main__':
         n= int(input("How much data do you want to enter? : "))
         
         for i in range(n):
-                print("please enter a date : ",end=" ")
-                date=input()
-                if not validate_date(date):
+                print("please enter a date : ", end=" ")
+                user_date=input()
+                if not validate_date(user_date):
                         print("Date is wrong ")
                         exit()
                 print("please enter a time: ", end=" ")
-                time=input()
-                if not validate_time(time):
+                user_time=input()
+                if not validate_time(user_time):
                         print("time is wrong")
                         exit()
-                data=" ".join([date,time])
-                data= date + " - " + time
+                data=" ".join([user_date,user_time])
+                data= user_date + " - " + user_time
                 my_list.append(data)
-        print("Thank you very much. I will notify them!")
+        date_time_now =validate_current_date_time()
+        
+        if date_time_now < my_list[i]:
+                        print("Thank you very much. I will notify them!")
 
-        for i in range(n):
-                print(f"The {i+1} data has been reached! ({my_list[i]})")
+                        for i in range(n):
+                          date_time_now =validate_current_date_time()
+                          if date_time_now < my_list[i]:
+                                  while(date_time_now!=my_list[i]):
+                                         time.sleep(1)
+                                         date_time_now=validate_current_date_time()  
+                                  message=print_result(i+1) 
+                                  print(message)
+        else:
+                        print("the {} date is wrong".format(i+1))
+                                   
+
+
+                
+
+        
+
     
 
  
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from task import validate_date,validate_time
+from task import print_result, validate_date,validate_time,print_result
 
 class TestInformation(unittest.TestCase):
     def test_date(self):
@@ -9,6 +9,10 @@ class TestInformation(unittest.TestCase):
     def test_time(self):
          self.assertEqual(validate_time("20:30"),True)
          self.assertEqual(validate_time("20:70"),False)
+
+    def test_print(self):
+          self.assertEqual(print_result(1),"The first data has been reached" ) 
+          self.assertNotEqual(print_result(2),"The 5 data has been reached")   
               
 
 
